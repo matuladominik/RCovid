0a7df75fb8f99caca86a83a7e516c063190b179e
Matula Dominik <dmatula@profinit.eu>
2020-03-29 21:44:26 +0200

    First aniamtions added - CZE covid19 by regions and GENDER or AGE.

--- covid19_infected_by_region.R
+++ covid19_infected_by_region.R
@@ -4,59 +4,122 @@ library("tidyverse")
 library("gganimate")
 library("transformr")  # sf + gganimate needs this
 library("RCzechia")
+library("tibbletime")  # because of rollify()
 
 source("data_utils.R")
+ggplot2::theme_set(ggplot2::theme_minimal())
 
-
-
-ggplot2::theme_set(ggplot2::theme_void())
 
 # get data
-infected <- load_data_individuals()
-regions <- 
-  RCzechia::kraje("low") %>% 
-  set_names(c("region_code", "region_nuts3", "region_name", "geometry")) %>% 
-  select(-region_code)
+infected_people <- load_data_individuals()
+regions <- get_czech_regions()
 
 
 # transform data
-daily_stats <- 
-  infected %>% 
-  filter(date_reported > "2020-03-05") %>% 
-  group_by(date_reported, region) %>% 
-  arrange(date_reported) %>% 
-  mutate(
-    running_mean_age = dplyr::cummean(age),
-    running_pct_male = dplyr::cummean(gender == "Male"),
-    tmp = 1,
-    running_count = cumsum(tmp)
-  ) %>% 
-  select(-tmp)
+daily_stats <- prepare_daily_stats(infected_people, since = "2020-03-01")
+
+running_daily_stats <- 
+  add_running_vars(daily_stats) %>% 
+  left_join(regions, by = c("region" = "region_nuts3"))
+
 
 
 
 # PLOTS -------------------------------------------------------------------
 
-daily_stats <- 
-  daily_stats %>% 
-  left_join(regions, by = c("region" = "region_nuts3"))
+
+# CASES reported by region and date
+
+running_daily_stats %>% 
+  ggplot(aes(x = date_reported, y = n_infected)) + 
+  geom_histogram(stat = "identity", alpha = .3) + 
+  geom_line(aes(y = rolling_mean_infected, col = "Moving avg (5 days window)"), size = 1) + 
+  facet_wrap("region_name") + 
+  labs(
+    x = "Date reported",
+    y = "# infected",
+    title = "Covid19: CASES reported by region",
+    col = NULL
+  ) + 
+  theme(legend.position = "bottom")
+
 
 
 # Gender percentage of infected (by date)
 
-daily_stats %>% 
-  filter(date_reported > "2020-03-25") %>% 
+p <- running_daily_stats %>% 
+  filter(date_reported >= "2020-03-15") %>% 
+  ggplot() + 
+  geom_hline(yintercept = .5, linetype = "dashed", alpha = .6, col = "firebrick3", size = 1) + 
+  geom_point(aes(x = region_name, y=running_pct_male, size = running_count), stat = "identity") + 
+  scale_y_continuous(breaks = seq(0, 1, .1), labels = scales::percent, limits = 0:1) + 
+  labs(
+    x = NULL,
+    y = "% of male patients",
+    title = "Covid19 - Proportion of male patients, by region",
+    subtitle = "Date till: {frame_time}"
+  ) + 
+  coord_flip() + 
+  transition_time(date_reported)
+
+gganimate::animate(p, end_pause = 100, fps = 25, duration = 15, width=400, height=600)
+
+
+# The same date, but plotted as a map
+
+running_daily_stats %>% 
+  filter(date_reported == "2020-03-15") %>% 
+  mutate(running_pct_male = ifelse(running_count < 10, NA, running_pct_male)) %>% 
   ggplot() + 
   geom_sf(aes(fill = running_pct_male, geometry = geometry)) + 
-  scale_x_continuous(breaks = NULL) + 
-  scale_fill_continuous(limits = c(0, 1), labels = scales::percent) + 
+  geom_sf_text(aes(geometry = label_pos, label = n_infected), size = 2.5)
+
+
+
+tol_min_cases <- 10
+info_text <- str_c("% of MALE patients, iff # CASES > ", tol_min_cases)
+
+p <- running_daily_stats %>% 
+  filter(date_reported >= "2020-03-01") %>% 
+  mutate(running_pct_male = ifelse(running_count < tol_min_cases, NA, running_pct_male)) %>% 
+  ggplot() + 
+  geom_sf(aes(fill = running_pct_male, geometry = geometry)) + 
+  scale_fill_gradient2(low = "#E63900", mid = "gray85", high = "deepskyblue3", midpoint = 0.5, na.value = "#FFFFFF00", limits = c(0, 1), labels = scales::percent) + 
   labs(
-    title = "Covid19:  % of male patients",
-    subtitle = "Date reported: 2020-03-25, cummulative stats",
-    fill = "% of MALE\npatients"
+    title = "Covid19: GENDER of patients by region (CZE)",
+    subtitle = str_c(info_text, "\nReported till: {frame_time}"),
+    fill = info_text
   ) + 
+  theme(legend.position = "bottom", legend.title.align = 1, axis.text = element_blank()) +
   transition_time(date_reported)
+
+gganimate::animate(p, end_pause = 10, fps = 5, duration = 10, width=600, height=400)
+
 
 
 # Age
 
+p <- 
+  running_daily_stats %>% 
+  filter(date_reported >= "2020-03-01") %>% 
+  mutate(running_mean_age = ifelse(running_count <= 10, NA, running_mean_age)) %>% 
+  ggplot() + 
+  geom_sf(aes(fill = running_mean_age, geometry = geometry), col = "gray77") + 
+  # geom_sf_text(aes(geometry = label_pos, label = n_infected), size = 2.5) + 
+  scale_fill_gradient(low = "gray33", high = "firebrick", na.value = "#FFFFFF00") + 
+  labs(
+    title = "Covid19 - Average age of patients",
+    subtitle = "Reported till: {frame_time}",
+    fill = "Average age of patients\niff # CASES > 10"
+  ) + 
+  theme(legend.position = "bottom", legend.title.align = 1, axis.text = element_blank()) +
+  transition_time(date_reported)
+
+gganimate::animate(p, end_pause = 10, fps = 5, duration = 10, width=600, height=400)
+
+
+
+
+# do dat je potřeba přidat 0
+# přidat info, že data nejsou k dispozici
+# přidat informaci o počtu nakažených (druhá osa y)
--- data_utils.R
+++ data_utils.R
@@ -1,10 +1,25 @@
 
 
+# load Covid19 data -------------------------------------------------------
+
+is_outdated <- function(file_path, max_age_secs = 3600) {
+  
+  mtime <- file.mtime(file_path)
+  now <- Sys.time()
+  
+  age_secs <- as.numeric(now - mtime, units = "secs")
+  age_secs > max_age_secs
+}
+
 
 download_data_individuals <- function(url = "https://onemocneni-aktualne.mzcr.cz/api/v1/covid-19/osoby.csv") {
   
   file_name <- base::basename(url)
   dest_path <- stringr::str_c("data/", file_name)
+  
+  if (file.exists(dest_path) && is_outdated(dest_path)) {
+    return(invisible(dest_path))
+  }
   
   curl::curl_download(url, destfile = dest_path, quiet = TRUE)   # TODO: add check if outdated
   invisible(dest_path)
@@ -45,3 +60,97 @@ load_data_individuals <- function() {
     )
 }
 
+
+# load other data ---------------------------------------------------------
+
+
+get_czech_regions <- function(resolution = "low") {
+  stopifnot(resolution %in% c("low", "high"))
+  regions <- 
+    RCzechia::kraje("low") %>% 
+    set_names(c("region_code", "region_nuts3", "region_name", "geometry")) %>% 
+    select(-region_code)
+  
+  adjust_label_pos_by <- function(regions, nuts3_code, by = c(0, 0)) {
+    which_point <- which(regions$region_nuts3 == nuts3_code)
+    regions$label_pos[which_point] <- regions$label_pos[which_point] + by
+    invisible(regions)    
+  }
+  
+  regions %>% 
+    add_czech_regions_population() %>% 
+    mutate(label_pos = sf::st_centroid(geometry)) %>% 
+    adjust_label_pos_by("CZ020", c(0.5, 0)) %>% 
+    adjust_label_pos_by("CZ071", c(0, -0.2))
+}
+
+
+
+add_czech_regions_population <- function(regions) {
+  # https://www.czso.cz/documents/10180/91917344/1300721901.xlsx/a0e0c274-c1d0-4e78-bf77-99a177538d3a?version=1.0
+  
+  region_popul <- tribble(
+    ~region_nuts3, ~popul, ~popul_male, ~popul_female, ~mean_age, ~mean_age_male, ~mean_age_female,
+    "CZ010", 1308632,	638009,	670623,	41.9,	40.5,	43.3,
+    "CZ020", 1369332,	676696,	692636,	41.2,	40.0,	42.4,
+    "CZ031", 642133,	317268,	324865,	42.7,	41.4,	43.9,
+    "CZ032", 584672,	290226,	294446,	42.7,	41.5,	43.9,
+    "CZ041", 294896,	145674,	149222,	42.9,	41.5,	44.2,
+    "CZ042", 820789,	407395,	413394,	42.0,	40.7,	43.4,
+    "CZ051", 442356,	217791,	224565,	42.1,	40.7,	43.5,
+    "CZ052", 551021,	271591,	279430,	43.1,	41.6,	44.5,
+    "CZ053", 520316,	257948,	262368,	42.4,	41.0,	43.8,
+    "CZ063", 509274,	253061,	256213,	42.8,	41.4,	44.1,
+    "CZ064", 1187667,	582516,	605151,	42.4,	40.9,	43.8,
+    "CZ071", 632492,	309620,	322872,	42.8,	41.2,	44.3,
+    "CZ072", 582921,	285883,	297038,	43.1,	41.4,	44.7,
+    "CZ080", 1203299,	590516,	612783,	42.7,	41.1,	44.2
+  )
+  regions %>% 
+    left_join(region_popul, by = "region_nuts3")
+}
+
+
+
+# preparing daily stats ---------------------------------------------------
+
+
+add_empty_days <- function(daily_stats) {
+  days <- unique(daily_stats$date_reported)
+  regions <- unique(daily_stats$region)
+  
+  crossing(date_reported = days, region = regions) %>% 
+    left_join(daily_stats, by = c("date_reported", "region")) %>% 
+    mutate_at(vars(-region, -date_reported), ~replace_na(., 0))
+}
+
+
+prepare_daily_stats <- function(infected_people, since = "2020-03-01") {
+  daily_stats <- 
+    infected_people %>% 
+    filter(date_reported > since) %>% 
+    group_by(date_reported, region) %>% 
+    summarise(
+      n_infected = n(),
+      n_male = sum(gender == "Male"),
+      sum_age = sum(age),
+      n_infected_abroad = sum(infected_abroad)
+    )
+  daily_stats %>% 
+    add_empty_days()
+}
+
+
+add_running_vars <- function(daily_stats) {
+  daily_stats %>% 
+    group_by(region) %>% 
+    arrange(date_reported) %>% 
+    mutate(
+      running_count = cumsum(n_infected),
+      running_mean_age = cumsum(sum_age)/running_count,
+      running_pct_male = cumsum(n_male)/running_count,
+      running_pct_infected_abroad = cumsum(n_infected_abroad)/running_count,
+      rolling_mean_infected = rollify(mean, window = 5)(n_infected)
+    ) %>% 
+    select(date_reported, region, running_count, n_infected, everything())
+}
